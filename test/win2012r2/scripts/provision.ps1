874cdd0c1dbd0521f723f9e96cfd3baaa1e23b73
Stefan Scherer <scherer_stefan@icloud.com>
2014-04-14 19:56:47 +0200

    tried provisioning and winrm

---
+++ test/win2012r2/scripts/provision.ps1
@@ -0,0 +1,6 @@
+$box = Get-ItemProperty -Path HKLM:SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName -Name "ComputerName"
+$box = $box.ComputerName.ToString().ToLower()
+
+Write-Host 'Hello ' $env:COMPUTERNAME ' = ' $box ' ?'
+
+
