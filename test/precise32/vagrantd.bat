324d888f8208f2663936a52b2c84f6bb46f8e388
StefanScherer <scherer_stefan@icloud.com>
2014-04-15 09:35:27 +0200

    vagrantd with counter

--- test/precise32/vagrantd.bat
+++ test/precise32/vagrantd.bat
@@ -1,1 +1,12 @@
-call vagrant %* --debug >vagrant-%1.log 2>&1
+@echo off
+setlocal EnableDelayedExpansion
+set Num=1
+if exist counter.txt (
+  for /f "delims=" %%x in (counter.txt) do set /A Num=%%x+1
+)
+echo %Num% >counter.txt
+set "formattedNum=00%Num%"
+set Num=!formattedNum:~-2!
+
+echo vagrant %* --debug >%Num%-vagrant-%1.log 2>&1
+call vagrant %* --debug >%Num%-vagrant-%1.log 2>&1
