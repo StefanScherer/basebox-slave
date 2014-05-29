1b20be8c896d6986aa3f73c5345e09e092d4098f
StefanScherer <scherer_stefan@icloud.com>
2014-05-29 18:07:47 +0200

    another manual installer

---
+++ ubuntu/install-vmware-workstation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# wget https://download2.vmware.com/software/wkst/VMware-Workstation-Full-9.0.3-1410761.x86_64.bundle
+# 403 forbidden
+# no eulas-agreed option, you have to install manually
+sudo sh ./VMware-Workstation-Full-9.0.3-1410761.x86_64.bundle --console --required
