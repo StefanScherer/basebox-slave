8f867cb54db9a14507fcc30b187b6b14cc73e874
StefanScherer <scherer_stefan@icloud.com>
2014-06-17 00:26:25 +0200

    only 1 executor

--- scripts/install-jenkins-slave.bat
+++ scripts/install-jenkins-slave.bat
@@ -79,7 +79,7 @@ call :heredoc html >%TEMP%\JenkinsSwarmClient.xml && goto next2
   <Actions Context="Author">
     <Exec>
       <Command>java.exe</Command>
-      <Arguments>-jar c:\jenkins\swarm-client.jar -autoDiscoveryAddress 172.16.32.255 -labels windows -fsroot c:\jenkins</Arguments>
+      <Arguments>-jar c:\jenkins\swarm-client.jar -autoDiscoveryAddress 172.16.32.255 -executors 1 -labels windows -fsroot c:\jenkins</Arguments>
     </Exec>
   </Actions>
 </Task>
@@ -87,7 +87,7 @@ call :heredoc html >%TEMP%\JenkinsSwarmClient.xml && goto next2
 
 rem IF Jenkins has security set, use following Arguments for scheduled task:
 rem add options -username and -password to swarm-client:
-rem       <Arguments>-jar c:\jenkins\swarm-client.jar -autoDiscoveryAddress 172.16.32.255 -username vagrant -password vagrant -labels windows -fsroot c:\jenkins</Arguments>
+rem       <Arguments>-jar c:\jenkins\swarm-client.jar -autoDiscoveryAddress 172.16.32.255 -username vagrant -password vagrant -executors 1 -labels windows -fsroot c:\jenkins</Arguments>
 
 rem Schedule start of swarm client at start of the machine (after next reboot)
 rem
