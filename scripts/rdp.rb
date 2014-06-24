796ed17850b4ca79c3cde64330dd35163ed2bfd2
StefanScherer <scherer_stefan@icloud.com>
2014-06-24 08:11:19 +0200

    added patch for vagrant 1.6.3

---
+++ scripts/rdp.rb
@@ -0,0 +1,40 @@
+require "pathname"
+require "tmpdir"
+
+require "vagrant/util/subprocess"
+
+module VagrantPlugins
+  module HostWindows
+    module Cap
+      class RDP
+        def self.rdp_client(env, rdp_info)
+          config = nil
+          opts   = {
+            "drivestoredirect:s"       => "*",
+            "full address:s"           => "#{rdp_info[:host]}:#{rdp_info[:port]}",
+            "prompt for credentials:i" => "1",
+            "username:s"               => rdp_info[:username],
+          }
+
+          # Create the ".rdp" file
+          config_path = Pathname.new(Dir.tmpdir).join(
+            "vagrant-rdp-#{Time.now.to_i}-#{rand(10000)}.rdp")
+          config_path.open("w+") do |f|
+            opts.each do |k, v|
+              f.puts("#{k}:#{v}")
+            end
+          end
+
+          # Build up the args to mstsc
+          args = [config_path.to_s]
+          if rdp_info[:extra_args]
+            args = rdp_info[:extra_args] + args
+          end
+
+          # Launch it
+          Vagrant::Util::Subprocess.execute("mstsc", *args)
+        end
+      end
+    end
+  end
+end
